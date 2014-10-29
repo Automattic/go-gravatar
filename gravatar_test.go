37fcc4d3d46dd1b34958d36d173b9a84b43ec8b8
Mohammad Jangda <batmoo@gmail.com>
2014-10-29 05:17:21 +0000

    Add more tests covering various Gravatar params

--- gravatar_test.go
+++ gravatar_test.go
@@ -9,16 +9,35 @@ func TestNewGravatarFromEmail(t *testing.T) {
 	emailHashed := "b48def645758b95537d4424c84d1a9ff"
 	g := NewGravatarFromEmail(email)
 	if g.Hash != emailHashed {
-		t.Error("got hash: %q; expected: %q", g.Hash, emailHashed)
+		t.Errorf("got hash: %q; expected: %q", g.Hash, emailHashed)
 	}
 }
 
 func TestGravatarGetUrl(t *testing.T) {
-	expectedUrl := "https://www.gravatar.com/avatar/b48def645758b95537d4424c84d1a9ff"
-	hash := "b48def645758b95537d4424c84d1a9ff"
-	g := NewGravatar()
-	g.Hash = hash
-	if url := g.GetUrl(); url != expectedUrl {
-		t.Error("got url: %q; expected: %q", url, expectedUrl)
+	tests := []struct {
+		hash     string
+		def      string
+		rating   string
+		size     int
+		expected string
+	}{
+		{"b48def645758b95537d4424c84d1a9ff", "", "", 0, "https://www.gravatar.com/avatar/b48def645758b95537d4424c84d1a9ff"},
+		{"b48def645758b95537d4424c84d1a9ff", "", "", 100, "https://www.gravatar.com/avatar/b48def645758b95537d4424c84d1a9ff?s=100"},
+		{"b48def645758b95537d4424c84d1a9ff", "404", "", 0, "https://www.gravatar.com/avatar/b48def645758b95537d4424c84d1a9ff?d=404"},
+		{"b48def645758b95537d4424c84d1a9ff", "http://wp.com/wp-includes/images/blank.gif", "", 0, "https://www.gravatar.com/avatar/b48def645758b95537d4424c84d1a9ff?d=http%3A%2F%2Fwp.com%2Fwp-includes%2Fimages%2Fblank.gif"},
+		{"b48def645758b95537d4424c84d1a9ff", "", "pg", 0, "https://www.gravatar.com/avatar/b48def645758b95537d4424c84d1a9ff?r=pg"},
+		{"b48def645758b95537d4424c84d1a9ff", "mm", "r", 200, "https://www.gravatar.com/avatar/b48def645758b95537d4424c84d1a9ff?d=mm&r=r&s=200"},
+	}
+
+	for _, test := range tests {
+		g := NewGravatar()
+		g.Hash = test.hash
+		g.Default = test.def
+		g.Rating = test.rating
+		g.Size = test.size
+
+		if url := g.GetUrl(); url != test.expected {
+			t.Errorf("got url: %q; expected: %q", url, test.expected)
+		}
 	}
 }
